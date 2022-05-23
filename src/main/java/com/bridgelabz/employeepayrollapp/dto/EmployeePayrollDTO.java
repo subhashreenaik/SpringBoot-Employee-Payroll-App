76ebdc61374ecd919585eb3987cd7d0b8ead6677
Subhashree naik <subhashree.naik@gmail.com>
2022-05-23 23:28:02 +0530

    Adding logging file in properties

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -1,18 +1,21 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class EmployeePayrollDTO {
 	
-
-
 public int salary;
 public String fullName;
 
-public EmployeePayrollDTO(String fullName, int salary) {
-	this.fullName=fullName;
-	this.salary=salary;
-}
+//public EmployeePayrollDTO(String fullName, int salary) {
+//	this.fullName=fullName;
+//	this.salary=salary;
+//}
 
 }
--- src/main/java/com/bridgelabz/employeepayrollapp/dto/ResponseDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/ResponseDTO.java
@@ -1,27 +1,19 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@AllArgsConstructor
 public class ResponseDTO {
 	String message;
     Object data;
 
     public ResponseDTO(String message, Object data) {
         this.message = message;
-        this.data = data;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public Object getData() {
-        return data;
-    }
-
-    public void setData(Object data) {
         this.data = data;
     }
 }
