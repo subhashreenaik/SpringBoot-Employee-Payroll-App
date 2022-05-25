d3dc174ac7e904cdab29e0acfcac2dac133109bc
Subhashree naik <subhashree.naik@gmail.com>
2022-05-25 13:51:49 +0530

    ALL Validation done in dto level

--- src/main/java/com/bridgelabz/employeepayrollapp/exceptions/EmployeePayrollExceptionalHandler.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/exceptions/EmployeePayrollExceptionalHandler.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,7 +13,10 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.bridgelabz.employeepayrollapp.dto.ResponseDTO;
 
+import lombok.extern.slf4j.Slf4j;
+
 @ControllerAdvice
+@Slf4j
 public class EmployeePayrollExceptionalHandler {
 
 	@ExceptionHandler(MethodArgumentNotValidException.class)
@@ -23,4 +27,13 @@ public class EmployeePayrollExceptionalHandler {
         ResponseDTO responseDTO = new ResponseDTO("Exception While Processing REST Request", errMesg);
         return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
     }
+	
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	public ResponseEntity<ResponseDTO> handleHttpMessageNotReadableException(HttpMessageNotReadableException exception ){
+		log.error("Invalid date format",exception);
+		ResponseDTO responseDTO = new ResponseDTO("Exception While Processing REST Request","Should have date in the format dd yy MM");
+		return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
+	}
+	
+	
 }
